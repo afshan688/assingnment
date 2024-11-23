789cec9d5827647e62b906e7ec8a1a6201f15bdf
afshan688 <afshanshakeel688@gmail.com>
2024-11-22 21:39:13 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,63 @@
+let name= "Afshan";
+let age = 27;
+const email = 'afshanshakeel688@gmail.com';
+let number = 34777777777;
+let insitute = 'Jawan Pakistan';
+let education = 'b.com';
+
+document.write(`Name:  ${name}<br> <br>`)
+document.write(`Age: ${age} <br> <br>`);
+document.write(`Email: ${email} <br><br>`);
+document.write(`Number: ${number} <br><br>`);
+document.write(`Institute: ${insitute} <br> <br>`);
+document.write(`Education: ${education}<br><br>`);
+
+// Q NO : 2 //
+
+
+
+let firstname= 'afshan';
+   {
+    let firstname ='sana';
+    console.log(firstname);
+   } 
+    console.log(firstname);
+    /////sana  ,afshan//////
+
+
+
+    function num(){
+        const nums= 10
+        console.log(nums)
+    }
+    num();
+    //10////
+
+
+   // func();
+     //console.log(func);
+
+    //var func=function (){
+      //  console.log('welcome user!!');
+     //};
+     //function abc(){
+
+    // }
+     //func is not a function////
+     
+     var studentName = "Ali";
+     var studentName = "Ahmed";
+     console.log(studentName);
+     //Ans:ahmed
+
+
+     let studentAge = 20;
+     studentAge = 21;
+     console.log(studentAge);
+     //update value 21
+
+
+     const studentRollNumber = 101;
+       studentRollNumber = 102; 
+       console.log(studentRollNumber);
+       //this is const variable not reassign
